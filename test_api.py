35c0e0c86fed36f4aa820a81925d625ad4d3cfa1
Abid <abdur.abid@madetech.com>
2025-01-27 14:07:05 +0000

    test green - response returns json

--- test_api.py
+++ test_api.py
@@ -1,5 +1,6 @@
 import pytest
 from app import app
+import json
 
 @pytest.fixture
 def test_app():
@@ -12,3 +13,7 @@ def client(test_app):
 def test_index_returns_200(client):
     res = client.get("/")
     assert res.status_code == 200
+
+def test_index_returns_json_data(client):
+    res = client.get("/")
+    assert True if json.loads(res.data) else False
