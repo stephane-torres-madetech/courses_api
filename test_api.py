c0adff8d37f61198d8cb560fb176a76cbce92b5b
Abid <abdur.abid@madetech.com>
2025-01-27 12:26:58 +0000

    test pass: index route with 200 status code

--- test_api.py
+++ test_api.py
@@ -1,2 +1,20 @@
-def test_test():
-    assert True
+import pytest
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello world!"
+
+@pytest.fixture
+def test_app():
+    yield app
+
+@pytest.fixture
+def client(test_app):
+    return test_app.test_client()
+
+def test_index_returns_200(client):
+    res = client.get("/")
+    assert res.status_code == 200
